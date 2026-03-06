43c18f8d1624e16c1394bd28ace1a9f76ea54322
Vicente Agüero <vicenteaguero@uc.cl>
2026-03-05 22:33:51 -0300

    feat(frontend): :art: add design tokens

---
+++ frontend/src/core/theme/tokens.ts
@@ -0,0 +1,15 @@
+export const SPACING = {
+  XS: "0.25rem",
+  SM: "0.5rem",
+  MD: "1rem",
+  LG: "1.5rem",
+  XL: "2rem",
+  XXL: "3rem",
+} as const;
+
+export const RADIUS = {
+  SM: "0.25rem",
+  MD: "0.5rem",
+  LG: "1rem",
+  FULL: "9999px",
+} as const;
